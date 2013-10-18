31932428b2379a5781b21b814d9ee235019c29b6
huihan <huihancarl@gmail.com>
2013-10-18 11:09:25 +0800

    Create Tips for Interview.java

---
+++ Tips for Interview.java
@@ -0,0 +1,7 @@
+1: Make sure that you really understand the question and give out some examples before write any code.
+
+2: If there is some thing uncertain, feel free to discuss it with the interviewer even just give up the interview.
+
+3: You must have the determination to solve the problem instead a good communication with interviewer.
+
+4: Take your time and accept that you can not work it out because you didn't prepare enough.
