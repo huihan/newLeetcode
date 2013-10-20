07368dac979c1e5783554d06924bef7900bda3bc
huihan <huihancarl@gmail.com>
2013-10-20 15:58:06 +0800

    Create 09degree_10min_01times_String_DPmatrix_Longest Palindromic Substring.java

---
+++ 09degree_10min_01times_String_DPmatrix_Longest Palindromic Substring.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int len = s.length();
+        if(len == 0) return "";
+        boolean[][] mat = new boolean[len][len];
+        for(int i = 0 ; i < len ; i++){
+          Arrays.fill(mat[i],false);
+          mat[i][i] = true;  
+        } 
+        int start = 0 , maxLength = 1;
+        for(int k = 1; k < len ; k++){
+            for(int i = 0 ; i < len -k ; i++){
+                if(s.charAt(i) != s.charAt(k+i)) continue;
+                if(k == 1) 
+                    mat[i][i+k] = true;
+                else mat[i][i+k] = mat[i+1][i+k-1];
+                if(mat[i][i+k]  && k > maxLength) {
+                    maxLength = k;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(i,k);
+    }
+}
